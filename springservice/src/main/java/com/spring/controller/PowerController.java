9224245e34d09150ba32330ea750bceb874d2b92
linhong.he@ecloud.com.cn <linhong.he@ecloud.com.cn>
2018-10-10 14:20:36 +0800

    引入Swagger-uii

--- springservice/src/main/java/com/spring/controller/PowerController.java
+++ springservice/src/main/java/com/spring/controller/PowerController.java
@@ -1,7 +1,13 @@
 package com.spring.controller;
 
+import com.spring.controller.RequestModel.PowerRequestModel;
+import com.spring.controller.ResponseModel.PowerResponseModel;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -14,14 +20,13 @@ import java.util.Map;
  * @Date: 2018/10/8 15:09
  * @Description:
  */
+@Api
 @Controller
 public class PowerController {
-    @RequestMapping("index1000")
-    public @ResponseBody List<Map<String,String>> index(){
-        List<Map<String,String>> list=new ArrayList<>();
-        Map<String,String> map=new HashMap<>();
-        map.put("test","test");
-        list.add(map);
-        return list;
+    @ApiOperation(value = "test")
+    @PostMapping("/index1000")
+    public @ResponseBody PowerResponseModel index(@RequestParam PowerRequestModel powerRequestModel){
+
+        return new PowerResponseModel("test");
     }
 }
