23609969c343d17cba91c56b3835771e77af824d
linhong.he@ecloud.com.cn <linhong.he@ecloud.com.cn>
2018-10-15 10:47:43 +0800

    security&jwt

--- springservice1/src/main/java/com/spring/Filter/JwtAuthenticationTokenFilter.java
+++ springservice1/src/main/java/com/spring/Filter/JwtAuthenticationTokenFilter.java
@@ -37,19 +37,21 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
-        String authHeader = request.getHeader("Authorization");
-        String tokenHead = "Bearer ";
-        if (authHeader != null && authHeader.startsWith(tokenHead)) {
-            String authToken = authHeader.substring(tokenHead.length());
+        String authHeader = request.getHeader("X-Auth-Token");
+        //String tokenHead = "Bearer ";
+        if (authHeader != null /*&& authHeader.startsWith(tokenHead)*/) {
+            String authToken = authHeader;
             String username = jwtTokenUtil.getUsernameFromToken(authToken);
             if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
                 UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
                 if (jwtTokenUtil.validateToken(authToken, userDetails)) {
                     UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                     authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 }
             }
+
         }
         chain.doFilter(request, response);
     }
